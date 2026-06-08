4218523ce66e4337d1ee07d899d3a427e02da098
VoyagerX21 <khakse2gaurav2003@gmail.com>
2026-06-08 16:18:20 +0000

    bug fixed in regeneration

--- app/routes/regenerate.py
+++ app/routes/regenerate.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, request
+from app.controllers.regenerate import regen
 
 regen_bp = Blueprint("regen", __name__)
 
 @regen_bp.route("/", methods=["POST"], strict_slashes=False)
 def main():
     if request.method.lower() == "post":
-        return
+        return regen()
